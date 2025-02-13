2dabe31be987aebfa32a7ddd196fcc3276eb981b
ofnanezn <ofnanezn@gmail.com>
2025-02-13 21:10:20 +0000

    Create BQ datasets

---
+++ infra/bigquery.tf
@@ -0,0 +1,22 @@
+resource "google_bigquery_dataset" "migrator-dataset-stg" {
+  dataset_id                  = "${replace(var.service_name, "-", "_")}_stg"
+  description                 = "Staging migration dataset"
+  location                    = var.region
+  default_table_expiration_ms = 86400000    # 1 day
+
+  access {
+    role          = "OWNER"
+    user_by_email = module.project.service_account_email
+  }
+}
+
+resource "google_bigquery_dataset" "migrator-dataset-tgt" {
+  dataset_id                  = "${replace(var.service_name, "-", "_")}_tgt"
+  description                 = "Target migration dataset"
+  location                    = var.region
+
+  access {
+    role          = "OWNER"
+    user_by_email = module.project.service_account_email
+  }
+}
