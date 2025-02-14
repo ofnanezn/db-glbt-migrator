88f19871e5e2676a345a82f0804532bcb8480765
ofnanezn <ofnanezn@gmail.com>
2025-02-14 03:39:26 +0000

    Add departments migration script and created bucket to store migration data

--- infra/bigquery.tf
+++ infra/bigquery.tf
@@ -2,7 +2,7 @@ resource "google_bigquery_dataset" "migrator-dataset-stg" {
   dataset_id                  = "${replace(var.service_name, "-", "_")}_stg"
   description                 = "Staging migration dataset"
   location                    = var.region
-  default_table_expiration_ms = 86400000    # 1 day
+  default_table_expiration_ms = 86400000 # 1 day
 
   access {
     role          = "OWNER"
@@ -11,9 +11,9 @@ resource "google_bigquery_dataset" "migrator-dataset-stg" {
 }
 
 resource "google_bigquery_dataset" "migrator-dataset-tgt" {
-  dataset_id                  = "${replace(var.service_name, "-", "_")}_tgt"
-  description                 = "Target migration dataset"
-  location                    = var.region
+  dataset_id  = "${replace(var.service_name, "-", "_")}_tgt"
+  description = "Target migration dataset"
+  location    = var.region
 
   access {
     role          = "OWNER"
