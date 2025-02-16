44bf3aecb4abac4aa6fad3ec40f0256abf6c05fe
ofnanezn <ofnanezn@gmail.com>
2025-02-16 18:01:18 +0000

    Add backup cronjob

---
+++ infra/cronjob.tf
@@ -0,0 +1,18 @@
+# Create a backup cronjob for each table
+resource "google_cloud_scheduler_job" "table-backup" {
+  for_each = toset(var.tables)  .
+  name             = "${each.value}-backup-scheduler"
+  description      = "Cronjob to backup ${each.value}"
+  schedule         = "0 0 * * *"
+  time_zone        = "America/New_York"
+  attempt_deadline = "300s"
+
+  retry_config {
+    retry_count = 1
+  }
+
+  http_target {
+    http_method = "POST"
+    uri         = "https://bq-glbt-migrator-508891629639.us-central1.run.app/${each.value}/backup"
+  }
+}
--- infra/vars/prod.tfvars
+++ infra/vars/prod.tfvars
@@ -2,6 +2,12 @@ project_id = "glbt-challenge-migrations"
 service_name = "bq-glbt-migrator"
 region = "us-central1"
 
+tables = [
+    "departments",
+    "jobs",
+    "hired_employees",
+]
+
 activate_apis = [
     "bigquery.googleapis.com",
     "compute.googleapis.com",
